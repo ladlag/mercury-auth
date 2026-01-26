482d95e21991d9db1212cba2a63a0119db4a1957
Z <z@yundev.com>
2026-01-26 09:49:26 +0800

    注视

--- src/main/resources/migration_add_password_encryption.sql
+++ src/main/resources/migration_add_password_encryption.sql
@@ -2,6 +2,6 @@
 -- Run this script if you have an existing tenants table
 
 ALTER TABLE tenants 
-ADD COLUMN password_encryption_enabled TINYINT(1) NOT NULL DEFAULT 0 COMMENT 'Whether RSA password encryption is enabled for this tenant',
-ADD COLUMN rsa_public_key TEXT COMMENT 'Base64 encoded RSA public key for password encryption',
-ADD COLUMN rsa_private_key TEXT COMMENT 'Base64 encoded RSA private key for password decryption';
+ADD COLUMN password_encryption_enabled TINYINT(1) NOT NULL DEFAULT 0 COMMENT '是否开启密码加密',
+ADD COLUMN rsa_public_key TEXT COMMENT '公钥',
+ADD COLUMN rsa_private_key TEXT COMMENT '私钥';
